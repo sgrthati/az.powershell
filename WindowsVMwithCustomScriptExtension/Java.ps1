2186b82836a12d675187efe6cf3347adfc955d29
Sagar <101870480+thatisagar@users.noreply.github.com>
2022-04-02 17:00:23 +0530

    Add files via upload

---
+++ WindowsVMwithCustomScriptExtension/Java.ps1
@@ -0,0 +1,14 @@
+﻿# Create Java folder.
+Write-Host "creating a directory"
+$path = New-Item -ItemType directory -Path C:\ -name "Java" -Force
+
+# Download JDK
+write-host "Downloading software" 
+$jdk_url = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245805_df5ad55fdd604472a86a45a217032c7d"
+
+$jdk_zip_file = "$path\jdk.exe"
+
+Invoke-WebRequest $jdk_url -OutFile $jdk_zip_file
+Write-Host "installing softaware"
+Start-Process $jdk_zip_file  /s -Wait
+Write-Host " software installation completed"
---
+++ WindowsVMwithCustomScriptExtension/Windows VM with Custom Extension Java.ps1
@@ -0,0 +1,35 @@
+﻿$RG = "SRI"
+$SUBNET1 = "SRI_SUBNET_1"
+$VNET = "SRI_VNET"
+$AVAILABILITYSET = "SRI_AVAILABILITY_SET"
+$VM = "SRIVM"
+$Count= "1"
+$location = "CentralIndia"
+
+#Resource Group Creation
+
+new-azresourcegroup -Name $RG -location "$location"
+
+#Vnet with Single subnet creation
+
+$SN1 = New-AzVirtualNetworkSubnetConfig -name $SUBNET1 -AddressPrefix "10.0.2.0/24"
+New-AzVirtualNetwork -name "$VNET" -ResourceGroupName "$RG" -Location "$location" -AddressPrefix "10.0.0.0/16" -subnet $SN1
+New-AzPublicIpAddress -Name "PIP$i" -ResourceGroupName "$RG" -Location "$location" -AllocationMethod Static -IpAddressVersion IPv4
+
+#Availability Set Creation
+
+New-AzAvailabilitySet   -Location "$location"   -Name $AVAILABILITYSET  -ResourceGroupName "$RG"   -Sku aligned  -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount 2
+
+
+
+
+#For Multiple VM Creation
+
+for ($i=1; $i -le $Count; $i++)
+{
+$VMLocalAdminUser = "sagar"
+$VMLocalAdminSecurePassword = ConvertTo-SecureString "Sagar@123456" -AsplainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword)
+New-AzVM -ResourceGroupName "$RG" -Name "$VM$i" -location "$location" -Image "Win2019Datacenter" -VirtualNetworkName "$VNET" -Subnetname "$SUBNET1" -Credential $Credential -Size "Standard_DS1_v2" -AvailabilitySetName "$AVAILABILITYSET" -PublicIpAddressName "PIP$i"
+Set-AzVMCustomScriptExtension -Name "test" -Location "$location" -ResourceGroupName "$RG" -VMName "$VM$i" -StorageAccountName '<storage account name>' -ContainerName '<contqainer name>' -FileName '<File Name>' -StorageAccountKey '<Storage account security key>' -Run '<file name>'
+} 
