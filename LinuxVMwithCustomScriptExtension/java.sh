29b397d022a71315c25674332eaef0b1fa3b10e4
Sagar <101870480+thatisagar@users.noreply.github.com>
2022-04-04 09:51:48 +0530

    Update java.sh

--- LinuxVMwithCustomScriptExtension/java.sh
+++ LinuxVMwithCustomScriptExtension/java.sh
@@ -2,4 +2,8 @@
 cls
 sudo apt update
 sudo apt upgrade
-sudo apt install -y default-jdk 
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
