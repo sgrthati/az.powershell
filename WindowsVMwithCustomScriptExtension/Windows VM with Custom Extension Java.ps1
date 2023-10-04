ad435c5ea10b459ab96c1e93de6b3be352e460c7
Sagar <101870480+sgrthati@users.noreply.github.com>
2023-10-04 17:43:02 +0530

    Update Windows VM with Custom Extension Java.ps1

--- WindowsVMwithCustomScriptExtension/Windows VM with Custom Extension Java.ps1
+++ WindowsVMwithCustomScriptExtension/Windows VM with Custom Extension Java.ps1
@@ -20,9 +20,6 @@ New-AzPublicIpAddress -Name "PIP$i" -ResourceGroupName "$RG" -Location "$locatio
 
 New-AzAvailabilitySet   -Location "$location"   -Name $AVAILABILITYSET  -ResourceGroupName "$RG"   -Sku aligned  -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount 2
 
-
-
-
 #For Multiple VM Creation
 
 for ($i=1; $i -le $Count; $i++)
@@ -31,5 +28,9 @@ $VMLocalAdminUser = "sagar"
 $VMLocalAdminSecurePassword = ConvertTo-SecureString "Sagar@123456" -AsplainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword)
 New-AzVM -ResourceGroupName "$RG" -Name "$VM$i" -location "$location" -Image "Win2019Datacenter" -VirtualNetworkName "$VNET" -Subnetname "$SUBNET1" -Credential $Credential -Size "Standard_DS1_v2" -AvailabilitySetName "$AVAILABILITYSET" -PublicIpAddressName "PIP$i"
+#to run custom script resided in storage account
 Set-AzVMCustomScriptExtension -Name "test" -Location "$location" -ResourceGroupName "$RG" -VMName "$VM$i" -StorageAccountName '<storage account name>' -ContainerName '<contqainer name>' -FileName '<File Name>' -StorageAccountKey '<Storage account security key>' -Run '<file name>'
+#to install windows IIS
+$PublicSettings = '{"commandToExecute":"powershell Add-WindowsFeature Web-Server"}'
+Set-AzVMExtension -ResourceGroupName "$RG" -ExtensionName "IIS" -VMName "$VM$i" -Location "$location" -Publisher "Microsoft.Compute" -ExtensionType "CustomScriptExtension" -TypeHandlerVersion "1.4" -SettingString $PublicSettings
 } 
