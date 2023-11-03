0c1c0d92973539fdc55d9dae3207380db855f1a5
Sagar <101870480+sgrthati@users.noreply.github.com>
2023-11-03 13:13:02 +0530

    Update Windows VM with Custom Extension Java.ps1

--- WindowsVMwithCustomScriptExtension/Windows VM with Custom Extension Java.ps1
+++ WindowsVMwithCustomScriptExtension/Windows VM with Custom Extension Java.ps1
@@ -27,9 +27,10 @@ for ($i=1; $i -le $Count; $i++)
 $VMLocalAdminUser = "sagar"
 $VMLocalAdminSecurePassword = ConvertTo-SecureString "Sagar@123456" -AsplainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword)
-New-AzVM -ResourceGroupName "$RG" -Name "$VM$i" -location "$location" -Image "Win2019Datacenter" -VirtualNetworkName "$VNET" -Subnetname "$SUBNET1" -Credential $Credential -Size "Standard_DS1_v2" -AvailabilitySetName "$AVAILABILITYSET" -PublicIpAddressName "PIP$i"
+New-AzPublicIpAddress -Name "PIP$i" -ResourceGroupName "$RG" -Location "$location" -AllocationMethod Static -IpAddressVersion IPv4
+New-AzVM -ResourceGroupName "$RG" -Name "$VM-$i" -Image "Win2022AzureEditionCore" -location "$location" -VirtualNetworkName "$VNET" -Subnetname "$SUBNET1" -Credential $Credential -Size "Standard_DS1_v2" -AvailabilitySetName "$AVAILABILITYSET" -PublicIpAddressName "PIP$i" -SecurityType "Standard" -OpenPorts 8080,3389,80
 #to run custom script resided in storage account
-Set-AzVMCustomScriptExtension -Name "test" -Location "$location" -ResourceGroupName "$RG" -VMName "$VM$i" -StorageAccountName '<storage account name>' -ContainerName '<contqainer name>' -FileName '<File Name>' -StorageAccountKey '<Storage account security key>' -Run '<file name>'
+Set-AzVMCustomScriptExtension -Name "test" -Location "$location" -ResourceGroupName "$RG" -VMName "$VM-$i" -StorageAccountName '<storage account name>' -ContainerName '<contqainer name>' -FileName '<File Name>' -StorageAccountKey '<Storage account security key>' -Run '<file name>'
 #to install windows IIS
 $PublicSettings = '{"commandToExecute":"powershell Add-WindowsFeature Web-Server -IncludeManagementTools"}'
 Set-AzVMExtension -ResourceGroupName "$RG" -ExtensionName "IIS" -VMName "$VM$i" -Location "$location" -Publisher "Microsoft.Compute" -ExtensionType "CustomScriptExtension" -TypeHandlerVersion "1.4" -SettingString $PublicSettings
